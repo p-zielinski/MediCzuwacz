0aac8fb4bb9f751a36128a44bae7d6ca376fcc4b
p-zielinski <piotrzielinski96@yahoo.com>
2025-06-30 17:04:11 +0200

    test

--- start.ts
+++ start.ts
@@ -1,37 +1,25 @@
 
-import { exec } from "node:child_process";
-import * as nodemailer from "nodemailer"
-import * as dotenv from "dotenv";
+const { exec } = require('child_process');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const emailAddress = process.env.EMAIL_ADDRESS
+const emailAddress:string = process.env.EMAIL_ADDRESS!
 
 const transporter = nodemailer.createTransport({
-    host: "smtppro.zoho.com",
-    secure: false, // Use `true` for port 465, `false` for all other ports
+    host: "smtp.zeptomail.com",
+    port: 587,
     auth: {
         user: process.env.SMTP_AUTH_USER,
-        pass: process.env.SMTP_AUTH_PASSWORD,
-    },
-    port: 587,
-    tls: {
-        rejectUnauthorized: true,
-        minVersion: "TLSv1.2"
+        pass: process.env.SMTP_AUTH_PASS
     }
 });
 
 
-const sendEmail = async(text) =>  {
-    console.log({
-        from: `"${emailAddress}" <${emailAddress}>`, // sender address
-        to: emailAddress!.split(','), // list of receivers
-        subject: `Medicover appointment available`, // Subject line
-        text: `Medicover appointment available`, // plain text body
-        html: text.replaceAll('\n','<br />'), // html body
-    })
+const sendEmail = async(text:string) =>  {
     // send mail with defined transport object
     const info = await transporter.sendMail({
-        from: `"${emailAddress}" <${emailAddress}>`, // sender address
+        from: `no-reply@project4.io`, // sender address
         to: emailAddress!.split(','), // list of receivers
         subject: `Medicover appointment available`, // Subject line
         text: `Medicover appointment available`, // plain text body
@@ -41,10 +29,10 @@ const sendEmail = async(text) =>  {
     console.log("Message sent: %s", info.messageId);
 }
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+const sleep = (ms:number) => new Promise(r => setTimeout(r, ms));
 
-const checkAppointment = async () => await new Promise((resolve) => {
-    return exec(process.env.MEDIHUNTER_RUN_COMMAND, (error, stdout, stderr) => {
+const checkAppointment = async ():Promise<string | undefined> => await new Promise((resolve) => {
+    return exec(process.env.MEDIHUNTER_RUN_COMMAND!, (errorL:any, stdout:any, stderr:any) => {
         if(stdout.includes('Date: ')){
             return resolve(stdout);
         }
@@ -58,11 +46,12 @@ const checkAppointment = async () => await new Promise((resolve) => {
 
 (async ()=>{
     while(true){
-        const appointments = 'xxxx'//await checkAppointment();
+        const appointments = await checkAppointment();
         if(appointments){
             await sendEmail(appointments).catch(console.error);
             await sleep(1000 * 90)
         }
+        console.log('No appointments found');
         await sleep(1000 * 90)
     }
 })()
